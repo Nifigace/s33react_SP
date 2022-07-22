1b7974056cc94fadf7a2a2495811bd921971edf3
Nifigace <53969580+Nifigace@users.noreply.github.com>
2022-07-22 08:17:28 +0300

    add "Calculator"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Head from "./Head.jsx";
 import List from "./List.jsx";
+import * as Calculator from "./Calculator.js";
 
 function App() {
   return (
     <div>
       <Head />
       <List />
+      <ul>
+        <li>{Calculator.add(1, 2)}</li>
+        <li>{Calculator.multiply(1, 2)}</li>
+        <li>{Calculator.subtract(1, 2)}</li>
+        <li>{Calculator.divide(1, 2)}</li>
+      </ul>
     </div>
   );
 }
