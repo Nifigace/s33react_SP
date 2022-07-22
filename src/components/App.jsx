6c69935f3eaa36b2585039112f6b775fc037fbb6
Nifigace <53969580+Nifigace@users.noreply.github.com>
2022-07-22 08:22:13 +0300

    variables were changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <List />
       <ul>
         <li>{Calculator.add(1, 2)}</li>
-        <li>{Calculator.multiply(1, 2)}</li>
-        <li>{Calculator.subtract(1, 2)}</li>
-        <li>{Calculator.divide(1, 2)}</li>
+        <li>{Calculator.multiply(2, 3)}</li>
+        <li>{Calculator.subtract(7, 2)}</li>
+        <li>{Calculator.divide(5, 2)}</li>
       </ul>
     </div>
   );
